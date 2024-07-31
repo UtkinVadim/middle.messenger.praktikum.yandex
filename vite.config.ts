6409a970191ed316552743c8a1d414641cc2df77
UtkinVadim <utkinvadim1991@gmail.com>
2024-07-31 22:10:49 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'static'),
   build: {
