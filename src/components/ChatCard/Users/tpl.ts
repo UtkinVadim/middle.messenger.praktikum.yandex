5ef67770a374569b1e3529bf2960c16d71ee49a3
UtkinVadim <utkinvadim1991@gmail.com>
2024-10-21 15:00:00 +0300

    Add 'add user' icon and update ChatCard Users layout

--- src/components/ChatCard/Users/tpl.ts
+++ src/components/ChatCard/Users/tpl.ts
@@ -1,12 +1,17 @@
 const tpl: string = `
+<div class="chat_card__users-add-user">
+
+</div>
 <p>Chat users:</p>
-<ul>
-  <li><span>Пункт 1</span></li>
-  <li><span>Пункт 2</span></li>
-  <li><span>Пункт 3</span></li>
-  <li><span>Пункт 4</span></li>
-  <li><span>Пункт 5</span></li>
-</ul>
+<div class="chat_card__users-list">
+  <ul>
+    <li><span>Пункт 1</span></li>
+    <li><span>Пункт 2</span></li>
+    <li><span>Пункт 3</span></li>
+    <li><span>Пункт 4</span></li>
+    <li><span>Пункт 5</span></li>
+  </ul>
+</div>
 `;
 
 export default tpl;
