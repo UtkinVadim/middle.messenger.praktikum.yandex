f0b00f90431ea3a84fda156225aebe1efe14268e
UtkinVadim <utkinvadim1991@gmail.com>
2024-10-21 15:33:34 +0300

    Refactor Users component for better attribute handling

--- src/components/ChatCard/Users/index.ts
+++ src/components/ChatCard/Users/index.ts
@@ -5,14 +5,26 @@ import Block from '../../../services/Block.ts';
 import type { PropsAndChildren } from '../../../types/Block.d.ts';
 
 interface IPropsAndChildrenUsers extends PropsAndChildren {
-    chatId: number;
+  chatId: number;
 }
 
 export default class Users extends Block {
   constructor(propsAndChildren: IPropsAndChildrenUsers, tagName: string = 'div') {
-    const props = {
-      ...propsAndChildren,
-      attr: {class: 'chat_card__users'}
+    const props = { ...propsAndChildren };
+
+    if (!props.attr) {
+      props.attr = {};
+    }
+
+    props.attr.class = 'chat_card__users';
+
+    if (!props.events) {
+      props.events = {};
+    }
+
+    props.events.click = function onClick(event: PointerEvent) {
+      event.preventDefault();
+      event.stopPropagation();
     };
 
     super(tagName, props);
--- src/components/ChatCard/Users/tpl.ts
+++ src/components/ChatCard/Users/tpl.ts
@@ -1,14 +1,12 @@
 const tpl: string = `
 <p>Chat users:</p>
-<div class="chat_card__users-list">
-  <ul>
-    <li><span>Пункт 1</span></li>
-    <li><span>Пункт 2</span></li>
-    <li><span>Пункт 3</span></li>
-    <li><span>Пункт 4</span></li>
-    <li><span>Пункт 5</span></li>
-  </ul>
-</div>
+<ul>
+  <li><span>Пункт 1</span></li>
+  <li><span>Пункт 2</span></li>
+  <li><span>Пункт 3</span></li>
+  <li><span>Пункт 4</span></li>
+  <li><span>Пункт 5</span></li>
+</ul>
 `;
 
 export default tpl;
