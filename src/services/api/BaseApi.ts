e00f74a10e1890e29965182e80a79fcd8cee700d
UtkinVadim <utkinvadim1991@gmail.com>
2024-11-04 14:57:47 +0300

    Type alias name to PascalCase

--- src/services/api/BaseApi.ts
+++ src/services/api/BaseApi.ts
@@ -1,14 +1,14 @@
 import HTTPTransport from '../HTTPTransport.ts';
 import type { MethodOptions } from '../../types/HTTPTransport.d.ts';
 
-type requestData = Record<string, any> | null;
+type RequestData = Record<string, any> | null;
 
 const http: HTTPTransport = new HTTPTransport();
 
 export default abstract class BaseAPI {
   protected _baseUrl: string = 'https://ya-praktikum.tech/api/v2';
 
-  protected static _getOptions(data: requestData = null): MethodOptions {
+  protected static _getOptions(data: RequestData = null): MethodOptions {
     const options: MethodOptions = {
       credentials: 'include',
       mode: 'cors',
@@ -19,25 +19,25 @@ export default abstract class BaseAPI {
     return options;
   }
 
-  protected _get(path: string, data: requestData = null): Promise<XMLHttpRequest> {
+  protected _get(path: string, data: RequestData = null): Promise<XMLHttpRequest> {
     const url = this._baseUrl + path;
     const options = BaseAPI._getOptions(data);
     return http.get(url, options);
   }
 
-  protected _post(path: string, data: requestData = null) {
+  protected _post(path: string, data: RequestData = null) {
     const url = this._baseUrl + path;
     const options = BaseAPI._getOptions(data);
     return http.post(url, options);
   }
 
-  protected _put(path: string, data: requestData = null) {
+  protected _put(path: string, data: RequestData = null) {
     const url = this._baseUrl + path;
     const options = BaseAPI._getOptions(data);
     return http.put(url, options);
   }
 
-  protected _delete(path: string, data: requestData = null) {
+  protected _delete(path: string, data: RequestData = null) {
     const url = this._baseUrl + path;
     const options = BaseAPI._getOptions(data);
     return http.delete(url, options);
